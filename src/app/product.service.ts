d31303b272ad553a27261ef4b4c12cb8d9a8ec6b
Walter Smart <walter@schuh.co.uk>
2019-03-19 08:51:18 +0000

    Completed part 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,6 +6,8 @@ import 'rxjs/add/operator/map'
 
 import { Album } from './album';
 
+import { Product } from './product';
+
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
@@ -13,10 +15,17 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
+  private _productsUrl = '../assets/products.json'
+
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
       <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) =>
+      <Product[]>response.json());
   }
 }
